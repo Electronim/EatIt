ef9ab9dab1a4d325a6e50d6f29d62695edc4131f
TooHappy22 <teodormarchitan@gmail.com>
2019-05-18 17:30:34 +0300

    Splash activity added

---
+++ EatItRemake/app/src/main/java/com/mh/eatitremake/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.mh.eatitremake;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashActivity extends Activity {
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_file);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainMenuActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
+            }
+        }, 1500);
+
+    }
+}
